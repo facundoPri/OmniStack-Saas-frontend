92a69f2e23385b96f97b25491e260eee9a485f18
facundo prieto <facundo.prieto321@gmail.com>
2020-05-22 20:44:26 -0300

    SignIn token creation api conection

--- src/pages/Auth/SignIn/index.js
+++ src/pages/Auth/SignIn/index.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
-// import { Container } from './styles';
+import { useDispatch } from 'react-redux';
+import AuthActions from '~/store/ducks/auth';
 
-function SignIn() {
-  return <div />;
+import Button from '~/styles/components/Button';
+import { Container, SignForm } from '../styles';
+
+export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSubmit = useCallback(
+    e => {
+      e.preventDefault();
+
+      dispatch(AuthActions.signInRequest(email, password));
+    },
+    [dispatch, email, password]
+  );
+
+  return (
+    <Container>
+      <SignForm onSubmit={handleSubmit}>
+        <h1>Boas vindas</h1>
+
+        <span>E-MAIL</span>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+
+        <span>SENHA</span>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+
+        <Button size="big" type="submit">
+          Entrar
+        </Button>
+      </SignForm>
+    </Container>
+  );
 }
-
-export default SignIn;
