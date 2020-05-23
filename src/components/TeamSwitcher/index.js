5716be3521c983c9918c9529b273b5f3f594f93f
facundo prieto <facundo.prieto321@gmail.com>
2020-05-23 00:53:32 -0300

    Selected team, TEAM header

--- src/components/TeamSwitcher/index.js
+++ src/components/TeamSwitcher/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import TeamsActions from '~/store/ducks/teams';
 
@@ -12,11 +12,18 @@ export default function TeamSwitcher() {
     dispatch(TeamsActions.getTeamsRequest());
   }, [dispatch]);
 
+  const handleTeamSelect = useCallback(
+    team => {
+      dispatch(TeamsActions.selectTeam(team));
+    },
+    [dispatch]
+  );
+
   return (
     <Container>
       <TeamList>
         {teams.data.map(team => (
-          <Team key={team.id}>
+          <Team key={team.id} onClick={() => handleTeamSelect(team)}>
             <img
               src={`https://ui-avatars.com/api/?font-size=0.33&background=7159c1&color=fff&name=${team.name}`}
               alt={team.name}
